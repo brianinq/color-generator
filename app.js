3f032da07f410d37666b8999eb20156f6a67df0f
brianinq <tayloroovse@gmail.com>
2021-04-29 10:16:17 +0300

    colourize sliders

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function genHex() {
     // return hash
 
     hexCode = chroma.random();
-    return hexCode
+    return hexCode;
 }
 console.log(colorDivs)
 
@@ -29,6 +29,17 @@ function randomColors() {
 
         //change color of displayed hex based on contrast
         checkContrast(hexText, randomColor)
+
+        //color slider
+
+        const color = chroma(randomColor)
+        const sliders = div.querySelectorAll('.sliders input')
+        const hue = sliders[0]
+        const brightness = sliders[1]
+        const saturation = sliders[2]
+        console.log(sliders)
+
+        colourizeSliders(color, hue, brightness, saturation);
     });
 }
 
@@ -40,5 +51,25 @@ function checkContrast(text, color) {
         text.style.color = '#fff';
     }
 }
+
+function colourizeSliders(color, hue, brightness, saturation) {
+    const noSaturation = color.set('hsl.s', 0)
+    const fullSaturation = color.set('hsl.s', 1)
+    // set saturation scale from low to the current color to high
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation])
+    // set input saturation range
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`
+
+    //btightness set scale
+    const midBrightness = color.set('hsl.l', 0.5)
+    // from dark to mid brightness to low light
+    const scaleBrightness = chroma.scale(['black', midBrightness, 'white'])
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0.5)}, ${scaleBrightness(1)} )`
+
+    //set hue (all RGBs)
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`
+}
+
+
 
 randomColors()
